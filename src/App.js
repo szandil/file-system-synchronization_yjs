17f81f4459d688bf86548934def7f68053efa06e
ladanyisz <uxqurt@INF.ELTE.HU>
2021-11-08 14:05:46 +0100

    react list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {createRef, useState} from "react";
+
+
+
 
 function App() {
-  return (
-    <div className="App">
 
+  const [array, setArray] = useState([]);
+  const arrayInputRef = createRef();
+
+  const addToArray = () => {
+    const newValue = arrayInputRef.current.value.trim();
+    if (newValue && newValue !== "" && !array.includes(newValue)) {
+      setArray([...array, newValue]);
+      arrayInputRef.current.value = "";
+    }
+  };
+
+  const onInputKeyDown = (e) => {
+    if (e.key === "Enter") {
+      addToArray();
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="container m-5">
+        <input
+          type="text"
+          className={`form-control mb-3 arrayInput`}
+          ref={arrayInputRef}
+          onKeyDown={onInputKeyDown}
+        />
+        <button className="btn btn-primary" onClick={addToArray}>Add to array</button>
+        <h4 className="mt-2">List of elements</h4>
+        <ul>
+          {array.map((element) =>
+            <li key={element}>{element}</li>
+          )}
+        </ul>
+
+      </div>
     </div>
   );
 }
