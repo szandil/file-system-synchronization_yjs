e5840e0fa80afccec3bf8edfdb323c1cf8cc11ba
ladanyisz <uxqurt@INF.ELTE.HU>
2021-11-08 14:29:46 +0100

    add yjs and yarray

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import {createRef, useState} from "react";
 
+import * as Y from "yjs";
 
+const doc1 = new Y.Doc();
 
 
 function App() {
 
+  const yarray = doc1.getArray("myarray");
   const [array, setArray] = useState([]);
   const arrayInputRef = createRef();
 
@@ -13,6 +16,8 @@ function App() {
     const newValue = arrayInputRef.current.value.trim();
     if (newValue && newValue !== "" && !array.includes(newValue)) {
       setArray([...array, newValue]);
+      yarray.push([newValue]);
+
       arrayInputRef.current.value = "";
     }
   };
@@ -23,23 +28,45 @@ function App() {
     }
   }
 
+  yarray.observe((event) => {
+    console.log("delta:", event.changes.delta);
+    console.log("yarray.toArray", yarray.toArray());
+  });
+
+
   return (
     <div className="container">
-      <div className="container m-5">
-        <input
-          type="text"
-          className={`form-control mb-3 arrayInput`}
-          ref={arrayInputRef}
-          onKeyDown={onInputKeyDown}
-        />
-        <button className="btn btn-primary" onClick={addToArray}>Add to array</button>
-        <h4 className="mt-2">List of elements</h4>
-        <ul>
-          {array.map((element) =>
-            <li key={element}>{element}</li>
-          )}
-        </ul>
+      <div className="row m-5">
+        <div className="col-6">
 
+          <input
+            type="text"
+            className={`form-control mb-3 arrayInput`}
+            ref={arrayInputRef}
+            onKeyDown={onInputKeyDown}
+          />
+          <button className="btn btn-primary" onClick={addToArray}>Add to react array</button>
+        </div>
+        <div className="row">
+
+          <div className="col-6">
+
+
+            <h4 className="mt-2">React array</h4>
+            <ul>
+              {array.map((element) =>
+                <li key={element}>{element}</li>
+              )}
+            </ul>
+          </div>
+          <div className="col-6">
+            <h4 className="mt-2">Yarray</h4>
+            <ul>
+              {yarray.toArray().map(element =>
+                <li key={`y${element}`}>{element}</li>)}
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
