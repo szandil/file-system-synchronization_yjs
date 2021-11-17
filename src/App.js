0f4eac25ee811970127cfd57ce302236c6d80752
ladanyisz <uxqurt@INF.ELTE.HU>
2021-11-17 18:57:33 +0100

    Treeview comment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import {useYProjectStructure} from "./useYProjectStructure";
+import {useEffect} from "react";
 
 function App() {
 
   const [data, projectMethods] = useYProjectStructure();
 
   // console.log('data1', data);
-  projectMethods.addFile('folder1/file1.js');
+  useEffect(() => {
+    projectMethods.addFile('folder1/file1.js');
+  }, []);
   console.log('data2', data);
 
+  // https://mui.com/components/tree-view/#basic-tree-view
+  // https://github.com/chenglou/react-treeview/blob/master/demos/controlled.js
   return (
-    <div></div>
+    <div className="container">
+        <div className="filenav">
+
+        </div>
+        <div className="main-content">Main</div>
+    </div>
   );
 }
 
--- src/useYProjectStructure.js
+++ src/useYProjectStructure.js
@@ -1,5 +1,5 @@
 import * as Y from "yjs";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 
 
 export function useYProjectStructure() {
@@ -14,36 +14,35 @@ export function useYProjectStructure() {
   yData.set('metadata', new Y.Map());
   yData.set('filesystem', new Y.Map());
 
-  useEffect(() => {
-    yData.observeDeep((event) => {
-      console.log(yData.toJSON());
-      setData(yData.toJSON());
-    });
-  });
 
-
-
-
-
-  // yData.observeDeep((event) => {
+  yData.observeDeep((event) => {
+    setData(yData.toJSON());
 
     // console.log('event', event);
 
     // let path = event[0].path;
     // let first = path.shift();
+    // console.log(path);
     // let last = path.length >= 1 ? path.pop() : first;
     // let newData = {};
     // let newDataParts = newData;
     // let yMap = yData.get(first);
-    // for(key in path) {
-    //   newDataParts[key] = {};
-    //   newDataParts = newDataParts[key];
-    //   yMap = yMap.get(key);
+    // for(let p in path) {
+    //   newDataParts[p] = {};
+    //   newDataParts = newDataParts[p];
+    //   yMap = yMap.get(p);
     // }
-    // newDataParts[key] = yMap.get(last);
+    // newDataParts[last] = yMap.get(last);
     // setData(prevData => {
     //   return {...prevData, first: newDataParts};
     // });
+
+    // console.log('yData',yData.toJSON());
+    // console.log('data',data);
+    // let dataCopy = data;
+    //
+    // buildObject(dataCopy, yData, path);
+    // console.log('done',dataCopy);
 
     // setData({data: 'blabla'});
 
@@ -51,7 +50,30 @@ export function useYProjectStructure() {
     // console.log(key, value);
     // console.log(event[0].path);
     // console.log(event[0].changes);
-  // });
+  });
+
+  // const buildObject = (dObj, yStructure, keys) => {
+  //   console.log('keys', keys)
+  //   if (keys.length === 0) return;
+  //   let key = keys.shift();
+  //   if (keys.length === 0 && key !== undefined) {
+  //     // console.log(dObj);
+  //     if (typeof yStructure.get(key) === "object") {
+  //       dObj[key]=yStructure.get(key).toJSON();
+  //       // console.log('if', dObj);
+  //     } else {
+  //       dObj[key]=yStructure.get(key);
+  //       console.log('else', dObj);
+  //     }
+  //     // return newObj;
+  //   } else {
+  //     let newYStructure = yStructure.get(key);
+  //     let newObj = dObj[key];
+  //     console.log('newystru0', newYStructure);
+  //     buildObject(newObj, newYStructure, keys);
+  //   }
+  //   // return newObj;
+  // };
 
   const projectMethods = {
 
@@ -78,9 +100,31 @@ export function useYProjectStructure() {
       } else {                        // new file
         fs.set(name, `${name}value`);           // TODO: a tartalma majd Y.Text
       }
-
     },
 
+    // deleteFile: (path) => {
+    //   let p = path.split('/');
+    //   let fs = yData.get('filesystem');
+    //
+    //   let last = p.pop();
+    //   for (let i = 0; i < p.length; i++) {
+    //     if (fs.has(p[i])) {
+    //       fs = fs.get(p[i]);
+    //     } else {
+    //       console.log('Delete failed - path not found!');
+    //       return;
+    //     }
+    //   }
+    //   if (!fs.has(last)) {
+    //     console.log('Delete failed - path not found!');
+    //     return;
+    //   } else {
+    //     if (last.split('.') === 1) {
+    //       this.delete(path + '/' + last);     // kell?
+    //     }
+    //     fs.delete(last);
+    //   }
+    // }
   };
 
   return [data, projectMethods];
